4907a93e0dd1b879a9ddb545efe43b2e8a7d4ce9
Andrew <mr.fesenko@inbox.ru>
2022-10-04 17:21:57 +0300

    added classwork

---
+++ 1HW/Classwork.py
@@ -0,0 +1,15 @@
+colors = ['red', 'green', 'blue']
+data = open('file.txt', 'a') #a=add, r=read, w=write(rewrite)
+data.writelines(colors)
+data.close()
+
+
+path = 'file.txt'
+data = open(path, 'r')
+for line in data:
+    print(line)
+data.close()
+
+import Task1 as T1
+
+print(T1.f(1))

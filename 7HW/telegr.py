18f1843702b1117f858619697bc5ef9263c4d51d
Andrew <mr.fesenko@inbox.ru>
2022-11-11 15:44:53 +0300

    added 7HW

--- 7HW/telegr.py
+++ 7HW/telegr.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_command import time_command, help_command, sum_command, hello
 
-app = ApplicationBuilder().token("5690321800:AAFWVK1MJdXr9QWFJzue9IcjeuBwPRibCpY").build()
+app = ApplicationBuilder().token("1").build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("time", time_command))
